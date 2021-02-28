e2dcebbd15017ef1239589f7aaef82c5809a62d3
Greyson Mainor <gbmainor98@outlook.com>
2021-02-27 19:29:20 -0500

    added other things to my generate markdown function.  Currently trying to figure out my promise functionality.

--- index.js
+++ index.js
@@ -56,26 +56,70 @@ function addPerson() {
                 ],
                 name: "anotherOne"
             }])
-            .then(function({moreInfo, anotherOne}){
-                let newPerson;
-                if (role === "Manager") {
-                    newPerson = new Manager(name, id, email, moreInfo);
-                } else if (role === "Engineer"){
-                    newPerson = new Engineer(name, id, email, moreInfo);
-                } else {
-                    newPerson = new Intern(name, id, email, moreInfo);
-                }
-                   employees.push(newPerson);
+                .then(function ({ moreInfo, anotherOne }) {
+                    let newPerson;
+                    if (role === "Manager") {
+                        newPerson = new Manager(name, id, email, moreInfo);
+                    } else if (role === "Engineer") {
+                        newPerson = new Engineer(name, id, email, moreInfo);
+                    } else {
+                        newPerson = new Intern(name, id, email, moreInfo);
+                    }
+                    employees.push(newPerson);
+                    generateMarkdownMid(newPerson)
+                    .then(function(){
+                        if (anotherOne === "yes") {
+                            addPerson();
+                        } else {
 
-                
-            })
+                        }
+                    })
+
+                })
         })
 };
 
+function generateMarkdownBegin() {
+    const html = `< !DOCTYPE html >
+  <html lang="en">
+  <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <title>Team Profile Generator</title>
+  </head>
+   <body>
+   <nav class="navbar navbar-dark bg-dark justify-content-center align-items-center">
+  <span class="navbar-brand mb-0 h1">
+  <h1>My Team</h1>
+  </span>
+  </nav>
+  <div class = "container">
+  <div class="row">`;
+  fs.writeFile("index.html", html, function(error){
+      if (err) {
+          console.log(error)
+      }
+  });
+  console.log("Begin");
+}
 
+function generateMarkdownMid(data) {
+return new Promise(function(resolve, reject){
+    
+})
+}
 
+function generateMarkdownEnd(){
+    const html = `</div>
+    </div>
+    </body>
+    </html>`;
+}
 function init() {
-        addPerson();
+    addPerson();
+    generateMarkdownEnd();
 }
 
 init();
