8542465b01ad641dbbd70b80dec7b29dde4ba304
Greyson Mainor <gbmainor98@outlook.com>
2021-02-27 19:13:30 -0500

    removed my generate markdown file.  Decided it might be easier to just have my generate markdown in my index.js instead of in a seperate file.

--- index.js
+++ index.js
@@ -1,51 +1,81 @@
 const inquirer = require("inquirer")
 const fs = require("fs")
 const generateMarkdown = require("./generateMarkdown")
+const { ENGINE_METHOD_RAND } = require("constants")
 const employees = []
 
-const questions = [
-    {
-        type: "input",
-        message: "Please input your name.",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "Please input your ID.",
-        name: "id"
-    },
-    {
-        type: "input",
-        message: "Please input your Email address.",
-        name: "email"
-    },
-    {
-        type: "list",
-        message: "Select a role for your user.",
-        choices: [
-            "Manager",
-            "Engineer",
-            "Intern"
-        ],
-        name: "role"
-    },
-    
-];
+function addPerson() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Please input your name.",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please input your ID.",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Please input your Email address.",
+            name: "email"
+        },
+        {
+            type: "list",
+            message: "Select a role for your user.",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern"
+            ],
+            name: "role"
+        },
 
-function writeToFile(fileName, data) { 
-    fs.writeFile(fileName,data, function(err) {
-        if (err){
-            return console.log(err);
-        } else
-       return console.log ("SUCCESS")
-    })
-}
+    ])
+        .then(function ({ name, id, email, role }) {
+            let moreInfo = "";
+            if (role === "Manager") {
+                moreInfo = "Office number";
+            } else if (role === "Engineer") {
+                moreInfo = "Github Username";
+            } else {
+                moreInfo = "school name"
+            }
+            inquirer.prompt([{
+                type: "input",
+                message: `Please enter ${moreInfo}`,
+                name: "moreInfo"
+            },
+            {
+                type: "list",
+                message: "Add another Member?",
+                choices: [
+                    "yes",
+                    "no"
+                ],
+                name: "anotherOne"
+            }])
+            .then(function({moreInfo, anotherOne}){
+                let newPerson;
+                if (role === "Manager") {
+                    newPerson = new Manager(name, id, email, moreInfo);
+                } else if (role === "Engineer"){
+                    newPerson = new Engineer(name, id, email, moreInfo);
+                } else {
+                    newPerson = new Intern(name, id, email, moreInfo);
+                }
+                   employees.push(newPerson);
+
+                
+            })
+        })
+};
+
+
 
 function init() {
-    inquirer.prompt(questions)
-    .then(function(data){
-        writeToFile("index.html", generateMarkdown(data));
-    })
- }
+        addPerson();
+}
 
- init();
+init();
